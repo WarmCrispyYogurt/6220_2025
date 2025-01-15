aae51e88e98d891377a12270b48efa0d5362278e
WarmCrispyYogurt <alexanderissoepic@gmail.com>
2025-01-15 15:59:59 -0800

    Added current state for the leds

--- src/main/java/frc/robot/subsystems/LEDCANdle.java
+++ src/main/java/frc/robot/subsystems/LEDCANdle.java
@@ -12,34 +12,41 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class LEDCANdle extends SubsystemBase {
   private final CANdle CANdle;
   private final CANdleConfiguration CANdleConfiguration;
+  private CurrentState currentState = CurrentState.OFF;
 
+  public enum CurrentState {
+      OFF,
+      ON,
+      RAINBOW
+  }
 // Initialize the LEDCANdle's configuration
-public LEDCANdle() {
-  // Creates a new CANdle with ID 0
-  this.CANdle = new CANdle(0);
-  this.CANdleConfiguration = new CANdleConfiguration();
-  this.CANdleConfiguration.stripType = LEDStripType.RGB;
-  this.CANdleConfiguration.statusLedOffWhenActive = false;
-  // Makes the CANdle not disable when it loses communication to the controller
-  this.CANdleConfiguration.disableWhenLOS = false;
-  // Can be set to 0.5 for half brightness, 1 for full brightness, etc.
-  this.CANdleConfiguration.brightnessScalar = 1;
-  this.CANdleConfiguration.vBatOutputMode = VBatOutputMode.Modulated;
-  // Configures all persistent settings
-  this.CANdle.configAllSettings(this.CANdleConfiguration, 100);
-}
+  public LEDCANdle() {
+    // Creates a new CANdle with ID 0
+    this.CANdle = new CANdle(0);
+    this.CANdleConfiguration = new CANdleConfiguration();
+    this.CANdleConfiguration.stripType = LEDStripType.RGB;
+    this.CANdleConfiguration.statusLedOffWhenActive = false;
+    // Makes the CANdle not disable when it loses communication to the controller
+    this.CANdleConfiguration.disableWhenLOS = false;
+    // Can be set to 0.5 for half brightness, 1 for full brightness, etc.
+    this.CANdleConfiguration.brightnessScalar = 1;
+    this.CANdleConfiguration.vBatOutputMode = VBatOutputMode.Modulated;
+    // Configures all persistent settings
+    this.CANdle.configAllSettings(this.CANdleConfiguration, 100);
+  }
 
   // Use RGB values to set the color of all the LEDs
   public void setColor(int r, int g, int b) {
     this.CANdle.setLEDs(r, g, b);
     // Modulates the VBat output to the specified duty cycle percentage
     this.CANdle.modulateVBatOutput(0.95);
+    this.currentState = CurrentState.ON;
 }
 
-@Override
-  public void periodic() {
-    SmartDashboard.putString("LED State", this.getCurrentState().toString());
-  }
+  @Override
+    public void periodic() {
+      SmartDashboard.putString("LED State", this.getCurrentState().toString());
+    }
 
     /** 
      * @param brightness
@@ -51,6 +58,7 @@ public LEDCANdle() {
     */
   public void setRainbow() {
     this.CANdle.animate(new RainbowAnimation(1, 0.5, 8));
+    this.currentState = CurrentState.RAINBOW;
   }
 
 
